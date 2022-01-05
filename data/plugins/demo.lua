48304da506b70d07494560917d242c02d5c96509
takase1121 <20792268+takase1121@users.noreply.github.com>
2022-01-05 14:57:56 +0800

    reset statusview, add window dimensions

--- data/plugins/demo.lua
+++ data/plugins/demo.lua
@@ -4,23 +4,32 @@ local config = require "core.config"
 
 -- find this file
 local this_file = DATADIR .. "/plugins/demo.lua"
-
 -- color dir
 local color_dir = system.absolute_path "colors"
-
 -- output dir
 local output_dir = system.absolute_path "previews"
-
 -- quit after demo
 local quit_after_demo = true
+-- window size
+local window_w, window_h = 1024, 576
 
 core.add_thread(function()
   local old_path = package.path
   package.path = package.path .. ";" .. color_dir .. PATHSEP .. "?.lua"
   config.transitions = false
+  config.message_timeout = 0
 
   -- open this file
   core.root_view:open_doc(core.open_doc(this_file))
+
+  -- clear statusview
+  core.status_view.message_timeout = 0
+
+  system.set_window_size(window_w, window_h, 0, 0)
+
+  core.status_view.message_timeout = 0
+
+  coroutine.yield(2 / config.fps)
 
   for _, file in ipairs(system.list_dir(color_dir) or {}) do
     local mod = file:gsub("%.lua$", "")
