0040727a1523135224d18f06567897378e5d93ff
takase1121 <20792268+takase1121@users.noreply.github.com>
2023-08-15 21:54:52 +0800

    docs(core.style): add documentation for styles

--- data/core/style.lua
+++ data/core/style.lua
@@ -1,17 +1,43 @@
 local common = require "core.common"
 local style = {}
 
+---The width of dividers between Views.
+---@type number
 style.divider_size = common.round(1 * SCALE)
+
+---The width of scrollbars when not expanded.
+---@type number
 style.scrollbar_size = common.round(4 * SCALE)
+
+---The width of scrollbars when expanded (during hover).
+---@type number
 style.expanded_scrollbar_size = common.round(12 * SCALE)
+
+---The width of the caret.
+---@type number
 style.caret_width = common.round(2 * SCALE)
+
+---The maximum width of a tab.
+---@type number
 style.tab_width = common.round(170 * SCALE)
 
+---The padding values between various UI elements, e.g. between buttons, icons and text.
+---
+---These values are general and thus widely used;
+---it is not recommended to change them directly unless the UI "looks wrong".
+---@type { x: number, y: number }
 style.padding = {
   x = common.round(14 * SCALE),
   y = common.round(7 * SCALE),
 }
 
+---The margin values for tabs.
+---@class core.style.margins.tabs
+---@field top number The top tab margin. This value can be negative.
+
+---The margin values between various UI elements.
+---@class core.style.margins
+---@field tab core.style.margins.tabs
 style.margin = {
   tab = {
     top = common.round(-style.divider_size * SCALE)
@@ -32,21 +58,45 @@ style.margin = {
 --
 -- On High DPI monitor or non RGB monitor you may consider using antialiasing grayscale instead.
 -- The antialiasing grayscale with full hinting is interesting for crisp font rendering.
+
+---The font used for most UI elements except DocView.
+---@type renderer.font
 style.font = renderer.font.load(DATADIR .. "/fonts/FiraSans-Regular.ttf", 15 * SCALE)
+
+---The font used for larger UI elements, e.g. The welcome screen.
+---@type renderer.font
 style.big_font = style.font:copy(46 * SCALE)
+
+---The font used for icons throughout the UI.
+---This should not be changed unless you know what you are doing.
+---@type renderer.font
 style.icon_font = renderer.font.load(DATADIR .. "/fonts/icons.ttf", 16 * SCALE, {antialiasing="grayscale", hinting="full"})
+
+---The font used for larger icons throughout the UI.
+---@type renderer.font
 style.icon_big_font = style.icon_font:copy(23 * SCALE)
+
+---The font used for DocView.
+---@type renderer.font
 style.code_font = renderer.font.load(DATADIR .. "/fonts/JetBrainsMono-Regular.ttf", 15 * SCALE)
 
+---A table containing colors for each syntax group.
+---
+---This table may contain colors for other undocumented syntax groups used by plugins.
+---@type { [core.syntax.syntax_group]: renderer.color }
 style.syntax = {}
 
--- This can be used to override fonts per syntax group.
--- The syntax highlighter will take existing values from this table and
--- override style.code_font on a per-token basis, so you can choose to eg.
--- render comments in an italic font if you want to.
+---A table of fonts that overrides the default font code for a syntax group.
+---
+---For instance, you could choose to render comments in italic by doing:
+---```lua
+---style.syntax_fonts["comment"] = style.code_font:copy(style.code_font:get_size(), { italic=true })
+---```
+---@type { [string]: renderer.font }
 style.syntax_fonts = {}
--- style.syntax_fonts["comment"] = renderer.font.load(path_to_font, size_of_font, rendering_options)
 
+---Colors for LogView entries.
+---@type { [core.logview.log_level]: renderer.color }
 style.log = {}
 
 return style
