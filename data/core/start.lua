005f5633c991d41d80e190cdd65b16cb26e768ef
takase1121 <20792268+takase1121@users.noreply.github.com>
2025-01-01 15:31:30 +0800

    add bytecode cache

--- data/core/start.lua
+++ data/core/start.lua
@@ -36,12 +36,79 @@ package.cpath =
   DATADIR .. '/?.' .. suffix .. ";" ..
   DATADIR .. '/?/init.' .. suffix .. ";"
 
+
+-- TODO: make this official?
+local CACHEDIR
+local cache_envvar = ""
+if     PLATFORM == "Linux"    then cache_envvar = "XDG_CACHE_HOME"
+elseif PLATFORM == "Windows"  then cache_envvar = "TMP"
+elseif PLATFORM == "Mac OS X" then cache_envvar = "TMPDIR" end
+local cachedir_paths = {
+  (PLATFORM == "Linux" and os.getenv(cache_envvar)) and os.getenv(cache_envvar) .. "/lite-xl/bytecode_cache" or false,
+  (PLATFORM == "Linux" and os.getenv("HOME"))       and os.getenv("HOME") .. "/.cache/lite-xl/bytecode_cache" or false,
+  (PLATFORM == "Linux" or PLATFORM == "Mac OS X")   and "/tmp/lite-xl/bytecode_cache" or false,
+  USERDIR .. "/bytecode_cache",
+}
+
+local common = require "core.common"
+for _, p in ipairs(cachedir_paths) do
+  if p and ((system.get_file_info(p) or {}).type == "dir" or common.mkdirp(p)) then
+    CACHEDIR = p
+    break
+  end
+end
+
 package.native_plugins = {}
-package.searchers = { package.searchers[1], package.searchers[2], function(modname)
+package.bytecode_cache = { { hit = 0, total = 0, dir = CACHEDIR } }
+
+local function slugify(name, stat)
+  return string.format(
+    "%s/%s@%s@%s.luac",
+    CACHEDIR,
+    name:gsub("^/?(.):?", "%1"):gsub("[%." .. PATHSEP .. "]", { ["."] = "%.", [PATHSEP] = "." }),
+    math.tointeger(math.floor(stat.modified * 1000)),
+    stat.size
+  )
+end
+
+local function cached_searcher(modname)
+  local modpath, err = package.searchpath(modname, package.path)
+  if not modpath then return err end
+  modpath = system.absolute_path(modpath)
+  local stat, err = system.get_file_info(modpath)
+  if not stat or stat.type ~= "file" then return string.format("cannot load %d: %s", modpath, err or "not a file") end
+  local slug = slugify(modpath, stat)
+  if not package.bytecode_cache[modpath] then package.bytecode_cache[modpath] = { name = slug, hit = 0 } end
+  local centry = package.bytecode_cache[modpath]
+
+  local fn, err = loadfile(slug, "b")
+  if not fn then
+    fn, err = loadfile(modpath)
+    if fn then
+      local cache_file, err = io.open(slug, "w")
+      if cache_file then
+        cache_file:write(string.dump(fn))
+        cache_file:close()
+      else
+        centry.error = err
+      end
+    end
+  else
+    centry.hit = centry.hit + 1
+    package.bytecode_cache[1].hit = package.bytecode_cache[1].hit + 1
+  end
+  package.bytecode_cache[1].total = package.bytecode_cache[1].total + 1
+  if not fn then return err end
+  return fn, modpath
+end
+
+local function lxl_native_api_searcher(modname)
   local path, err = package.searchpath(modname, package.cpath)
   if not path then return err end
   return system.load_native_plugin, path
-end }
+end
+
+package.searchers = { package.searchers[1], os.getenv("LITE_XL_BYTECODE_CACHE") == "0" and package.searchers[2] or cached_searcher, lxl_native_api_searcher }
 
 table.pack = table.pack or pack or function(...) return {...} end
 table.unpack = table.unpack or unpack
